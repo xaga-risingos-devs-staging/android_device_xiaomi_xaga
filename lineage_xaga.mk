0930bf9814975046987a44025080a38b2695d6ad
Hecheng Yu <kde-yyds@qq.com>
2025-04-16 14:14:57 +0000

    xaga: Add Chipest and Maintainer properties for RisingOS

--- lineage_xaga.mk
+++ lineage_xaga.mk
@@ -36,3 +36,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Redmi/xaga/xaga:12/SP1A.210812.016/OS2.0.7.0.ULOCNXM:user/release-keys \
     DeviceProduct=xaga
+
+# RisingOS
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Dimensity 8100" \
+    RisingMaintainer="kde-yyds"
